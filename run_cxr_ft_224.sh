bd06f6ea2497f7b68bd532dd2314512d24bb4304
Kang Liu <719176062@qq.com>
2025-08-08 20:50:43 +0800

    Update run_cxr_ft_224.sh

--- run_cxr_ft_224.sh
+++ run_cxr_ft_224.sh
@@ -3,19 +3,17 @@
 python main_224.py \
 --task finetune \
 --data_name mimic_cxr \
---mimic_cxr_ann_path "/home/20031211471/Code/Code/Data/mimic_cxr_annotation_sen_best_reports_keywords_20_multiview_individual_v0331.json" \
+--mimic_cxr_ann_path "/mimic_cxr_annotation_sen_best_reports_keywords_20_multiview_individual_v0331.json" \
 --ft_monitor_metric RCB \
 --version ft_v0411_v0416 \
 --max_seq_len 100 \
 --epochs 50 \
---load "/home/20031211471/Code/Code/Second-Multiview/results/mimic_cxr/pretrain/v0411_v0416_pt/checkpoint/model_best.pth" \
---is_multiview_learning \
---is_add_indication \
---sk_type keywords \
+--load "results/mimic_cxr/pretrain/v0411_v0416_pt/checkpoint/model_best.pth" \
+--is_multiview_learning "yes" \
+--is_add_indication "yes" \
 --pt_lr 5.0e-6 \
 --ft_lr 5.0e-5 \
---sk_topk 0 \
 --num_layers 3 \
 --optim RAdam \
---is_save_checkpoint \
+--is_save_checkpoint "yes" \
 --batch_size 32
