0d05a26a50c0641f9573642812d9c208647f728c
Kang Liu <719176062@qq.com>
2025-08-08 20:53:20 +0800

    Update run_cxr_test_224.sh

--- run_cxr_test_224.sh
+++ run_cxr_test_224.sh
@@ -3,12 +3,12 @@
 python main_224.py \
 --task "test" \
 --data_name mimic_cxr \
---mimic_cxr_ann_path "/home/miao/data/dataset/MIMIC-CXR/mimic_cxr_annotation_sen_best_reports_keywords_20_multiview_individual_v0331.json" \
+--mimic_cxr_ann_path "MIMIC-CXR/mimic_cxr_annotation_sen_best_reports_keywords_20_multiview_individual_v0331.json" \
 --ft_monitor_metric RCB \
 --version ft_v0411_v0416 \
 --max_seq_len 100 \
 --epochs 50 \
---load "/home/miao/data/Code/EVOKE/results/mimic_cxr/finetune/ft_v0411_v0416/checkpoint/model_best.pth" \
+--load "https://huggingface.co/datasets/MK-runner/Multi-view-CXR/blob/main/checkpoints/MIMIC-CXR/resolution-224x224/model_best.pth" \
 --is_multiview_learning "yes" \
 --is_add_indication "yes" \
 --pt_lr 5.0e-6 \
