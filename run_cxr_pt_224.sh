a47503ca3f5e015225c296fc951c766b9a55fa82
Kang Liu <719176062@qq.com>
2025-08-08 20:51:53 +0800

    Update run_cxr_pt_224.sh

--- run_cxr_pt_224.sh
+++ run_cxr_pt_224.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-python main_224.py \.py \
+python main_224.py \
 --task pretrain \
 --data_name mimic_cxr \
---mimic_cxr_ann_path "/home/20031211471/Code/Code/Data/mimic_cxr_annotation_sen_best_reports_keywords_20_multiview_individual_v0331.json" \
+--mimic_cxr_ann_path "/Data/mimic_cxr_annotation_sen_best_reports_keywords_20_multiview_individual_v0331.json" \
 --version v0411_v0416_pt \
 --max_seq_len 100 \
 --epochs 50 \
 --lr 5.0e-5 \
---sk_type keywords \
---multiple_positives_contrastive supcon \
 --optim AdamW \
---is_save_checkpoint \
---is_multiview_learning \
+--is_save_checkpoint "yes" \
+--is_multiview_learning "yes" \
 --batch_size 32
