adb5ae400cfdd79378c48b5df6f39d9f0e3c406e
Kang Liu <719176062@qq.com>
2025-08-08 20:54:00 +0800

    Update run_cxr_test_384.sh

--- run_cxr_test_384.sh
+++ run_cxr_test_384.sh
@@ -3,12 +3,12 @@
 python main_384.py \
 --task 'test' \
 --data_name mimic_cxr \
---mimic_cxr_ann_path "MIMIC-CXR/mimic_cxr_annotation_sen_best_reports_keywords_20_multiview_individual_v0331.json" \
+--mimic_cxr_ann_path "mimic_cxr_annotation_sen_best_reports_keywords_20_multiview_individual_v0331.json" \
 --ft_monitor_metric RCB \
 --version ft_v0623_large \
 --max_seq_len 100 \
 --epochs 50 \
---load "EVOKE/results/mimic_cxr/finetune/ft_v0623_large/checkpoint/model_best.pth" \
+--load "https://huggingface.co/datasets/MK-runner/Multi-view-CXR/blob/main/checkpoints/MIMIC-CXR/resolution-384x384/model_best.pth" \
 --is_multiview_learning "yes" \
 --is_add_indication "yes" \
 --pt_lr 5.0e-6 \
